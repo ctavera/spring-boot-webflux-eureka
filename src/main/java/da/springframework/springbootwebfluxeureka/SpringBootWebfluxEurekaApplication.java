1a9c8a17234c0d91d8fcdb9f6e23a2e6b3ba9c8d
Christian Tavera <christian.taveram@icloud.com>
2022-07-22 23:50:33 -0500

    101. Creating Eureka Server of Register. Initial Commit

---
+++ src/main/java/da/springframework/springbootwebfluxeureka/SpringBootWebfluxEurekaApplication.java
@@ -0,0 +1,15 @@
+package da.springframework.springbootwebfluxeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringBootWebfluxEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxEurekaApplication.class, args);
+    }
+
+}
